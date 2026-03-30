b9405e934a2476b19a0f36dd7200b01535eece2d
Kozan Morimoto <kozan@Kozans-MacBook-Pro.local>
2026-03-30 12:41:53 +0900

    added testimonials section and back to top btn

--- script.js
+++ script.js
@@ -48,3 +48,24 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll('.reveal').forEach(item => {
     observer.observe(item);
 });
+
+const backToTopButton = document.querySelector("#backToTop");
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopButton.classList.add("show");
+    } else {
+        backToTopButton.classList.remove("show");
+    }
+}
+
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Creates a prestigious, slow glide back to the top
+    });
+});
